c0bfc6a4477667571e9526d288ab6b19ae11b0d6
Piash <nazem.piash10@gmail.com>
2021-02-06 13:06:36 +0600

    posts crud design

--- resources/js/app.js
+++ resources/js/app.js
@@ -19,7 +19,22 @@ window.Vue = require('vue');
 // const files = require.context('./', true, /\.vue$/i)
 // files.keys().map(key => Vue.component(key.split('/').pop().split('.')[0], files(key).default))
 
-Vue.component('example-component', require('./components/ExampleComponent.vue').default);
+// Vue.component('example-component', require('./components/ExampleComponent.vue').default);
+
+import App from './App.vue';
+import VueRouter from 'vue-router';
+import VueAxios from 'vue-axios';
+import axios from 'axios';
+import {routes} from './routes';
+
+Vue.use(VueRouter);
+Vue.use(VueAxios, axios);
+
+const router = new VueRouter({
+    mode: 'history',
+    routes: routes
+});
+
 
 /**
  * Next, we will create a fresh Vue application instance and attach it to
@@ -29,4 +44,7 @@ Vue.component('example-component', require('./components/ExampleComponent.vue').
 
 const app = new Vue({
     el: '#app',
+    router: router,
+    render: h => h(App),
+
 });
